21f99177e3d885e7cfe8bd2aafacdbdf21dd7529
Negar Sarani <negar.sarani2000@gmail.com>
2023-02-05 00:46:38 +0330

    test2

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 "use strict";
 
-function counterMaker(params){
+// function counterMaker() {
+//   let count = 0;
+//   return function name(params = 0) {
+//     return (count += params);
+//   };
+// }
+// const counter = counterMaker();
+// console.log(counter()); // 0
+// console.log(counter()); // 0
+// console.log(counter(1)); // 1
+// console.log(counter()); // 1
+// console.log(counter(3)); // 4
+// console.log(counter(7)); // 11
+// console.log(counter(-5)); // 6
+
+function counterMaker() {
     let count = 0;
-   return function name() {
-        // eval(count + params)
-        console.log(count); 
-    }
-}
-const counter = counterMaker()
-// console.log( counter() ) // 0
-// console.log( counter() ) // 0
-console.log( counter(1) ) // 1
-// console.log( counter() ) // 1
-// console.log( counter(3) ) // 4
-// console.log( counter(7) ) // 11
-// console.log( counter(-5) ) // 6
+    return function name(params = 0) {
+      console.log(count += params);
+    };
+  }
+  const counter = counterMaker();
+  console.log(counter()); // 0
+  console.log(counter()); // 0
+  console.log(counter(1)); // 1
+  console.log(counter()); // 1
+  console.log(counter(3)); // 4
+  console.log(counter(7)); // 11
+  console.log(counter(-5)); // 6
+  
