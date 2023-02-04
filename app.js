3faa50df3e165d779caece50ea993eecc453c809
Negar Sarani <negar.sarani2000@gmail.com>
2023-02-04 18:06:30 +0330

    test1

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 "use strict";
+
+function counterMaker(params){
+    let count = 0;
+   return function name() {
+        // eval(count + params)
+        console.log(count); 
+    }
+}
+const counter = counterMaker()
+// console.log( counter() ) // 0
+// console.log( counter() ) // 0
+console.log( counter(1) ) // 1
+// console.log( counter() ) // 1
+// console.log( counter(3) ) // 4
+// console.log( counter(7) ) // 11
+// console.log( counter(-5) ) // 6
