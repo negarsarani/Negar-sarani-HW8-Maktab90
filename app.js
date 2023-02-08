c83d3f3b76ed9dc2268d761a4e81a6aa63e300d5
Negar Sarani <negar.sarani2000@gmail.com>
2023-02-08 22:40:17 +0330

    create folder question1

--- app.js
+++
@@ -1,9 +0,0 @@
-const user = {};
-function setterGenerator(key) {
-  return function name(value) {
-    return this[key] = value;
-  };
-}
-const nameSetter = setterGenerator("name");
-nameSetter.call(user,"jack");
-console.log(user); // {name: ‘jack’}
