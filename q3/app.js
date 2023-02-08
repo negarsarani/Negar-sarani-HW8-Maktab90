c9e1665dd21ea12209e601a357f7008b9bbf054d
Negar Sarani <negar.sarani2000@gmail.com>
2023-02-08 22:47:14 +0330

    create folder question3

---
+++ q3/app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function counterMaker() {
+    let count = 0;
+    return function name(params = 0) {
+      return count += params
+    };
+  }
+  const counter = counterMaker();
+  console.log(counter()); // 0
+  console.log(counter()); // 0
+  console.log(counter(1)); // 1
+  console.log(counter()); // 1
+  console.log(counter(3)); // 4
+  console.log(counter(7)); // 11
+  console.log(counter(-5)); // 6
+  
